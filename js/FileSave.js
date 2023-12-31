d8ee5a5b6933344494c2e28c6717528fd6c2c2dd
bettinocamarda <155069515+bettinocamarda@users.noreply.github.com>
2023-12-31 14:36:00 +0100

    Add files via upload

--- js/FileSave.js
+++ js/FileSave.js
@@ -2,15 +2,15 @@
  * A saveAs() FileSaver implementation.
  * 2014-11-29
  *
- * By Bettino Camarda, https://www.threads.net/@bettino.camarda
+ * By Eli Grey, http://eligrey.com
  * License: X11/MIT
- *   See https://github.com/bettinocamarda/FileSaver.js/blob/master/LICENSE.md
+ *   See https://github.com/eligrey/FileSaver.js/blob/master/LICENSE.md
  */
 
 /*global self */
 /*jslint bitwise: true, indent: 4, laxbreak: true, laxcomma: true, smarttabs: true, plusplus: true */
 
-/*!  */
+/*! @source http://purl.eligrey.com/github/FileSaver.js/blob/master/FileSaver.js */
 
 var saveAs = saveAs
   // IE 10+ (native saveAs)
